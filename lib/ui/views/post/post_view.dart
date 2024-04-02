5bb29492dcea9ba038d1380cf40d258dda7d9c2e
Talha Mehmood <130683337+Mehmoodtalha@users.noreply.github.com>
2024-04-03 00:09:20 +0500

    update ek vari fyr

--- lib/ui/views/post/post_view.dart
+++ lib/ui/views/post/post_view.dart
@@ -14,11 +14,91 @@ class PostView extends StackedView<PostViewModel> {
   ) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        title: Text("Add Post"),
-      ),
-      body: Container(
-        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      // appBar: AppBar(
+      //   title: Text("Add Post"),
+      // ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              GestureDetector(
+                onTap: viewModel.pickImage,
+                child: Container(
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: 
+                  // viewModel.image != null
+                  //     ? Image.file(viewModel.image?? , fit: BoxFit.cover)
+                  //     : 
+                      Icon(Icons.add_a_photo, size: 50, color: Colors.grey),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: viewModel.postCtrl,
+                decoration: InputDecoration(
+                  hintText: 'Enter your post...',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: null,
+              ),
+              
+              SizedBox(height: 20),
+              TextField(
+                controller: viewModel.companyCtrl,
+                decoration: InputDecoration(
+                  hintText: 'Enter Menufecturer...',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: null,
+              ),
+              
+              SizedBox(height: 20),
+              TextField(
+                controller: viewModel.modelCtrl,
+                decoration: InputDecoration(
+                  hintText: 'Enter Model...',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: null,
+              ),
+              
+              SizedBox(height: 20),
+              TextField(
+                controller: viewModel.milageCtrl,
+                decoration: InputDecoration(
+                  hintText: 'Enter milage...',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: null,
+              ),
+              
+              SizedBox(height: 20),
+              TextField(
+                controller: viewModel.priceCtrl,
+                decoration: InputDecoration(
+                  hintText: 'Enter Price...',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: null,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Here you can implement your logic to submit the post
+                  // You can use _textEditingController.text to get the text
+                  // And _image to get the selected image file
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/ui/views/post/post_viewmodel.dart
+++ lib/ui/views/post/post_viewmodel.dart
@@ -1,3 +1,30 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:stacked/stacked.dart';
 
-class PostViewModel extends BaseViewModel {}
+class PostViewModel extends BaseViewModel {
+  TextEditingController postCtrl= TextEditingController();
+  
+  TextEditingController companyCtrl= TextEditingController();
+  
+  TextEditingController modelCtrl= TextEditingController();
+  
+  TextEditingController milageCtrl= TextEditingController();
+  
+  TextEditingController priceCtrl= TextEditingController();
+   File? image;
+   Future<void> pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    
+      if (pickedFile != null) {
+        image = File(pickedFile.path);
+        notifyListeners();
+      } else {
+        print('No image selected.');
+      }
+    
+  }
+}
