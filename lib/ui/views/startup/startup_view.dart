2726e599bf0021e9efcccab9ce5cbadee65a3339
Talha Mehmood <130683337+Mehmoodtalha@users.noreply.github.com>
2024-04-04 14:03:17 +0500

    logo update

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -14,32 +14,34 @@ class StartupView extends StackedView<StartupViewModel> {
     StartupViewModel viewModel,
     Widget? child,
   ) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'STACKED',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
-            ),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Loading ...', style: TextStyle(fontSize: 16)),
-                horizontalSpaceSmall,
-                SizedBox(
-                  width: 16,
-                  height: 16,
-                  child: CircularProgressIndicator(
-                    color: Colors.black,
-                    strokeWidth: 6,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+        child: 
+        Image.asset('assets/logo.png'),
+        // Column(
+        //   mainAxisSize: MainAxisSize.min,
+        //   children: [
+        //     Text(
+        //       'STACKED',
+        //       style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+        //     ),
+        //     Row(
+        //       mainAxisSize: MainAxisSize.min,
+        //       children: [
+        //         Text('Loading ...', style: TextStyle(fontSize: 16)),
+        //         horizontalSpaceSmall,
+        //         SizedBox(
+        //           width: 16,
+        //           height: 16,
+        //           child: CircularProgressIndicator(
+        //             color: Colors.black,
+        //             strokeWidth: 6,
+        //           ),
+        //         )
+        //       ],
+        //     ),
+        //   ],
+        // ),
       ),
     );
   }
